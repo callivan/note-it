8c8edfbbe272502cc8bd8808a4a0aef3dfae373d
callivan <ivan-my-job@mail.ru>
2024-03-09 22:51:19 +0700

    Добавляет alias, меняет структуру папок под FSD

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true, node: 'current' },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:sonarjs/recommended',
-        'prettier',
-    ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', 'sonarjs', 'simple-import-sort'],
-    rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        'simple-import-sort/imports': 'error',
-        'no-console': 'warn',
-        'no-unused-vars': 'warn',
-        'sort-imports': 'off',
-        'import/order': 'off',
-    },
-}
+  root: true,
+  env: { browser: true, es2020: true, node: 'current' },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:sonarjs/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'sonarjs', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'simple-import-sort/imports': 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'sort-imports': 'off',
+    'import/order': 'off',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/01_app'),
+      '@pages': path.resolve(__dirname, './src/02_pages'),
+      '@widgets': path.resolve(__dirname, './src/03_widgets'),
+      '@features': path.resolve(__dirname, './src/04_features'),
+      '@entities': path.resolve(__dirname, './src/05_entities'),
+      '@shared': path.resolve(__dirname, './src/06_shared'),
+    },
+  },
   plugins: [react()],
-})
+});
